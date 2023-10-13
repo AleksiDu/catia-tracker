0361e88e6d950fcc02525a96debea79590ac57d1
AleksiDu <aleksi.duluzauri@gmail.com>
2023-10-13 14:26:00 +0400

    feat: add the new content after the first line

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import moment from "moment";
 import fs from "fs";
 
 let startTime: moment.Moment | null;
+
 const checkFor = async (processName: string) => {
   try {
     const processes = await si.processes();
@@ -20,19 +21,24 @@ const checkFor = async (processName: string) => {
       const formattedDate = moment().format("MM/DD/YYYY");
       const formattedRuntime = `${runtimeInSeconds} seconds`;
 
-      const tableContent =
-        `| Process Name | Start Time       | Runtime       |\n` +
-        `|--------------|------------------|---------------|\n` +
-        `| ${processName}    | ${formattedDate}       | ${formattedRuntime}    |\n`;
+      const tableRow = `
+      |--------------|------------------|---------------|
+      | ${processName}    | ${formattedDate}       | ${formattedRuntime}    |\n`;
 
       let existingContent = "";
       if (fs.existsSync("runtime.txt")) {
         existingContent = await fs.promises.readFile("runtime.txt", "utf-8");
       }
 
-      const updatedContent = existingContent + tableContent;
+      if (existingContent.length > 0) {
+        const lines = existingContent.split("\n");
+        existingContent =
+          lines[0] + "\n" + tableRow + lines.slice(2).join("\n");
+      } else {
+        existingContent = `| Process Name | Start Time       | Runtime       |\n${tableRow}`;
+      }
 
-      await fs.promises.writeFile("runtime.txt", updatedContent);
+      await fs.promises.writeFile("runtime.txt", existingContent);
       startTime = null;
     } else {
       console.log(`${processName} not started yet.`);
@@ -43,22 +49,3 @@ const checkFor = async (processName: string) => {
 };
 
 setInterval(() => checkFor("CNEXT.exe"), 10000);
-
-// const programStartTime = moment();
-
-// async function trackRuntime() {
-//   try {
-//     const programEndTime = moment();
-//     const runtimeInSeconds = programEndTime.diff(programStartTime, "seconds");
-
-//     // Save the runtime to a file
-//     await fs.promises.writeFile("runtime.txt", `${runtimeInSeconds} seconds`);
-
-//     console.log(`Program ran for ${runtimeInSeconds} seconds.`);
-//   } catch (error) {
-//     console.error("Error tracking runtime:", error);
-//   }
-// }
-
-// // Call the function to track the runtime
-// trackRuntime();
